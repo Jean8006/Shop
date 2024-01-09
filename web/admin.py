7397759a878bbff9f7b7ce86aff4aca95c78b070
Jean8006 <134330719+Jean8006@users.noreply.github.com>
2024-01-09 14:58:29 -0600

    modulo 1 listo

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Categoria,Producto
 # Register your models here.
+
+admin.site.register(Categoria)
+#admin.site.register(Producto)
+
+#decorador para utilizar funciones especiales es lo que se muestra en productos en el panel de administracion
+@admin.register(Producto)
+class ProductoAdmin(admin.ModelAdmin):
+    #se usa tuplas
+    list_display = ('nombre', 'precio', 'categoria', 'fecha_registro')
+    list_editable = ('precio',) 
